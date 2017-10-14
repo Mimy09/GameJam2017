16a47b3fe63dc1e4f21593c317214040a78270e7
Mitch <mjmt997@gmail.com>
2017-10-14 12:24:30 +1100

    Player Gun and Animation

--- GameJam2017_RP/Assets/Scripts/Player/Player.cs
+++ GameJam2017_RP/Assets/Scripts/Player/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour {
 
     /* ---- Player Vars ---- */
     public GameObject m_arm;
+    public GameObject m_body;
 
     /* ---- Player Movement Vars ---- */
     public int m_speed = 1;
@@ -29,6 +30,7 @@ public class Player : MonoBehaviour {
     //////////////////////////////////////////
     // Private Variables
     private Rigidbody2D m_rigidbody2D;
+    private Animator m_animator;
     private bool m_canJump = true;
     private bool m_grounded = false;
     private bool m_collidingRight = false;
@@ -36,13 +38,18 @@ public class Player : MonoBehaviour {
     private bool m_collidingLadder = false;
     private bool m_fliped = false;
     private float move;
+    private float angle;
     //////////////////////////////////////////
 
-
+    public float ArmAngle
+    {
+        get { return angle; }
+    }
 
     void Start() {
         // Get the rigidbody
         m_rigidbody2D = this.GetComponent<Rigidbody2D>();
+        m_animator = m_body.GetComponent<Animator>();
     }
      
     void Update() {
@@ -56,6 +63,10 @@ public class Player : MonoBehaviour {
         m_collidingLadder = Physics2D.OverlapCircle(m_groundCheck.position, m_groundRadius, m_LadderLayer);
         // Get movement
         move = Input.GetAxis("Horizontal");
+
+        // Animations
+        if (move != 0) m_animator.SetBool("Walking", true);
+        else m_animator.SetBool("Walking", false);
 
         // Move left and right
         if (move < 0 && !m_collidingLeft) {
@@ -87,19 +98,32 @@ public class Player : MonoBehaviour {
         object_pos = Camera.main.WorldToScreenPoint(m_arm.transform.position);
         mouse_pos.x = mouse_pos.x - object_pos.x;
         mouse_pos.y = mouse_pos.y - object_pos.y;
-        float angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
-        m_arm.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 90));
+        angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+        if (angle < 90 && angle > -90)
+            m_arm.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        else
+            m_arm.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 180));
 
         /* ---- Player Flipping ---- */
-        for (int i = 0; i < m_graphics.transform.childCount; i++) {
-            if (angle < 90 && angle > -90) {
-                m_graphics.transform.GetChild(i).GetComponent<SpriteRenderer>().flipX = m_fliped = false;
-            } else {
-                m_graphics.transform.GetChild(i).GetComponent<SpriteRenderer>().flipX = m_fliped = true;
-            }
+        if (angle < 90 && angle > -90) {
+           // m_arm.GetComponent<SpriteRenderer>().flipY = m_fliped = false;
+
+            Vector3 scale = m_graphics.transform.localScale;
+            scale.x = 1;
+            m_graphics.transform.localScale = scale;
+
+            //m_body.GetComponent<SpriteRenderer>().flipX = false;
+        } else {
+           // m_arm.GetComponent<SpriteRenderer>().flipY = m_fliped = true;
+
+            Vector3 scale = m_graphics.transform.localScale;
+            scale.x = -1;
+            m_graphics.transform.localScale = scale;
+
+            //m_body.GetComponent<SpriteRenderer>().flipX = true;
         }
 
-            /* ---- Jumping ---- */
+        /* ---- Jumping ---- */
         if (Input.GetKeyDown(KeyCode.Space)) { // JUMP
             if (m_grounded) m_rigidbody2D.AddForce(transform.up * m_jumpheight, ForceMode2D.Impulse);
         }
@@ -114,12 +138,5 @@ public class Player : MonoBehaviour {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireCube((m_groundCheck.right * m_wallColliderOffst) + transform.position, new Vector3(m_wallCollider.x, m_wallCollider.y, 0));
         Gizmos.DrawWireCube((-m_groundCheck.right * m_wallColliderOffst) + transform.position, new Vector3(m_wallCollider.x, m_wallCollider.y, 0));
-
-        Gizmos.color = Color.green;
-        if (m_fliped) {
-            Gizmos.DrawLine(m_groundCheck.position, (-m_groundCheck.right * 0.7f) + transform.position);
-        } else {
-            Gizmos.DrawLine(m_groundCheck.position, (m_groundCheck.right * 0.7f) + transform.position);
-        }
     }
 }
