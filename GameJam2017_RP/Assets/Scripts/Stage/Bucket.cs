f18074e1b1bb02323af22851bfd6eddbaa60c644
Francisco Romano <blastsilver@gmail.com>
2017-10-14 19:17:05 +1100

    Finished Stage 1

---
+++ GameJam2017_RP/Assets/Scripts/Stage/Bucket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : MonoBehaviour
+{
+    public int m_waterLimit = 5;
+    public bool m_isComplete = false;
+    public Door m_lockedDoorScript = null;
+    
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("sup!");
+        // check if complete
+        if (m_isComplete) return;
+        // check for player bullet
+        if (collider.tag == "Bullet")
+        {
+            Debug.Log("YESSSSSSSS!");
+            // update values
+            m_waterLimit--;
+            // check if empty
+            if (m_waterLimit == 0)
+            {
+                // set as complete
+                m_isComplete = true;
+                // unlock the door
+                m_lockedDoorScript.m_isUnlocked = true;
+            }
+        }
+    }
+}
---
+++ GameJam2017_RP/Assets/Scripts/Stage/Door.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // variables [public]
+    public bool m_isUnlocked = false;
+    [Range(0, 2)] public float m_speed = 0.5f; 
+    // variables [private]
+    private bool m_isOpen = false;
+    private float m_frameCount = 0;
+    private Vector3 m_offset;
+    private Vector3 m_position1;
+    private Vector3 m_position2;
+    private Vector3 m_targetPosition;
+    private bool m_isAnimationFinished = true;
+
+    void Start ()
+    {
+        // initialize variables
+        m_offset = new Vector3();
+        m_position1 = transform.position;
+        m_position2 = transform.position + Vector3.up * transform.localScale.y * 2;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // check if animation finished
+		if (!m_isAnimationFinished)
+        {
+            // check frame count
+            if (m_frameCount > 0)
+            {
+                // translate door
+                m_frameCount -= Time.deltaTime;
+                transform.Translate(m_offset * Time.deltaTime);
+            }
+            else
+            {
+                // stop animation
+                m_isAnimationFinished = true;
+                transform.position = m_targetPosition;
+            }
+        }
+	}
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        // check if unlocked
+        if (!m_isUnlocked || !m_isOpen) return;
+        // set boolean
+        m_isOpen = false;
+        m_isAnimationFinished = false;
+        // calculate animation info
+        m_frameCount = m_speed - m_frameCount;
+        m_targetPosition = m_position1;
+        m_offset = (m_position1 - transform.position) / m_frameCount;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // check if unlocked
+        if (!m_isUnlocked || m_isOpen) return;
+        // set boolean
+        m_isOpen = true;
+        m_isAnimationFinished = false;
+        // calculate animation info
+        m_frameCount = m_speed - m_frameCount;
+        m_targetPosition = m_position2;
+        m_offset = (m_position2 - transform.position) / m_frameCount;
+    }
+}
